2c92cc5cacc8075bc275d77605a68b4b51c20bc1
Dhivas-Sugumar <dhivassugumar@gmail.com>
2024-04-13 20:05:17 -0400

    WIP

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import mongoose from "mongoose";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
+import UserRoutes from "./Users/routes.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
@@ -11,4 +13,6 @@ Hello(app)
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 app.listen(process.env.PORT || 4000);
