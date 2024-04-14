8f164888c905f4912dc2f678cae45f74226ade36
Dhivas-Sugumar <dhivassugumar@gmail.com>
2024-04-13 20:15:29 -0400

    Done with regular stuff

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import session from "express-session";
+import "dotenv/config";
 import mongoose from "mongoose";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
@@ -7,8 +9,28 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+ );
+ app.use(express.json());
+ const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+
 Hello(app)
 Lab5(app);
 CourseRoutes(app);
