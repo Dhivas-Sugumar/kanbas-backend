933ead35988eb58a5c0e24f1feb2b73c5767dcc1
samblock <block.sa@northeastern.edu>
2024-04-19 16:43:28 -0400

    added ability for multiple answers

--- Kanbas/questions/schema.js
+++ Kanbas/questions/schema.js
@@ -5,7 +5,7 @@ const questionSchema = new mongoose.Schema({
     points: { type: Number, required: true },
     question: { type: String, required: true },
     questionType: { type: String, enum: ["Multiple Choice", "True/False", "Multiple Blanks"], default: "Multiple Choice" },
-    correctAnswer: { type: String, required: true },
+    correctAnswers: { type: [String], required: true },
     choices: { type: [String], default: [] },
     quizId: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz', required: true }, // Reference to the Quiz model
     quizOrder: { type: Number, required: true }},
