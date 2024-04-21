8ebf579fc64c4d98a9192a197af86f407b6d56b7
samblock <block.sa@northeastern.edu>
2024-04-21 18:53:37 -0400

    redid backend to represent questions as list in quiz schema to make question CRUD easier to handle

--- Kanbas/quizzes/schema.js
+++ Kanbas/quizzes/schema.js
@@ -4,8 +4,6 @@ const quizSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     quizType: { type: String, enum: ["Graded Quiz", "Practice Quiz", "Graded Survey", "Ungraded Survey"], default: "Graded Quiz" },
-    points: { type: Number, required: true, default: 0 },
-    numberOfQuestions: { type: Number, required: true, default: 0 },
     assignmentGroup: { type: String, enum: ["Quizzes", "Exams", "Assignments", "Project"], default: "Quizzes" },
     shuffleAnswers: { type: Boolean, default: true },
     timeLimit: { type: Number, default: 20 }, // in minutes
@@ -19,7 +17,17 @@ const quizSchema = new mongoose.Schema({
     availableDate: { type: Date, required: true },
     untilDate: { type: Date, required: true },
     published: { type: Boolean, default: false },
-    course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true } // Reference to the Course model
+    course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true }, // Reference to the Course model
+    questions: [
+        { title: String,
+        points: Number,
+        question: String,
+        questionType: { type: String, enum: ["multipleChoice", "trueFalse", "multipleBlanks"], default: "multipleChoice" },
+        correctAnswers: [String],
+        choices: [String],
+        quizOrder: Number,
+    createdAt: String,} 
+    ],
 }, { collection: "quizzes" });
 
 export default quizSchema;
